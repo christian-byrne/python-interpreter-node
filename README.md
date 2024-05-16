20d7367c2c54d98e48d2acecfe89d9509a0fed66
christian-byrne <abolkonsky.rem@gmail.com>
2024-05-15 18:40:12 -0700

    refactor TensorWrapper class. add docs, examples

--- README.md
+++ README.md
@@ -1,14 +1,145 @@
 # Python Interpreter ComfyUI Node
 
-- For debugging, parsing data, generating random values, converting types, testing custom nodes faster.
-- Reference and use variables in the code using the same names as the inputs in the UI
+**Description**:
+
+- Allows you to write Python code in the textarea of the node. When the node is executed, the code is run in a Python interpreter. 
+
+- The stdout (includes print() statements, errors, evals, etc.) is displayed in the node. 
+
+- The input values can be accessed by name in the code. The output values can be set by assigning to them in the code.
+
+![alt text](wiki/pictures/demos/p1-zoom.png)
+
+![alt text](wiki/pictures/demos/p1.png)
+
+
+
+**Uses**:
+
+- Debugging
+  - Testing custom nodes faster.
+- Doing any task that can be accomplished with Python code but you don't have a node for
+- Saving the node's state as a part of workflow effectively saves mini scripts as nodes in the workflow
+- Using logical operators to change the behavior of the queue workflow dynamically
+- Converting types
+  - Circumventing issues where different nodes provide values in different types and you need to add conditional conversions or formatting
+- Doing math with input values
+
+**Wrapper Class with Operator Overloading**:
+
+![alt text](wiki/pictures/demos/p2.png)
+
 - Wrapper class around tensors with operator overloading for doing common image manipulation tasks.I might remove this aspect
 
-![alt text](p1-zoom.png)
 
-![alt text](p1.png)
+# Example Uses
+
+## Accessing Stateful Variables at Time of Node Execution
+
+#### Get the most recent image in a folder
+
+```python
+import os
+import random
+
+files = os.listdir("/path/to/folder")
+files.sort(key=os.path.getmtime)
+
+# Choose a random second image with the condition that it must be the same size
+sample_files = [f for f in files if os.path.getsize(f) == os.path.getsize(files[1])]
+paired_file = random.choice(sample_files)
+
+image2.to(paired_file) # Wrapper class will handle conversion to tensor automatically
+```
+
+## Type Conversion
+
+#### Convert between Number Types with Printed Debug Info
+
+```python
+print(f"The type of number1 when it was received was {type(number1)}")
+
+if str(number1).isnumeric():
+    number1.to(int(number1))
+else:
+    print(f"number1 was not a number. It was {number1}")
+
+# Maybe change to 0 if it's negative (e.g., you are padding image1 to match image2 size but image1 is larger than in one dimension)
+if number1 < 0:
+    number1.to(0)
+```
+
+#### Color Format Conversion that Always Works
 
 
-![alt text](p2.png)
+
+```python
+import cv2
+hex_color = text1 # text1 is the first text input in the node
+
+def hex_to_rgb(hex):
+    hex = hex.lstrip("#")
+    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+
+# We can even add type checks to prevent frustration or inconsistencies with other node outputs
+if not isinstance(hex_color, str):
+    hex = str(hex_color)
+
+red, green, blue = hex_to_rgb(hex_color)
+
+# Output integer values representing r, g, b
+number1.to(int(red))
+number2.to(int(green))
+number3.to(int(blue))
+```
+
+
+
+
+## Image Manipulation
+
+## Debugging
+
+
+## Logical Operators / Conditional Execution
+
+
+## Math
+
+
+## Scripts for Specific Workflow Tasks
+
+#### Get Complementary Colors from an Image
+
+```python
+from sklearn.cluster import KMeans
+
+pixels = image1.view(-1, image1.shape[-1]).numpy() # torch already imported
+kmeans = KMeans(n_clusters=2, random_state=0).fit(pixels)
+main_colors = kmeans.cluster_centers_
+complement_colors = [(255 - main_colors[i]) % 256 for i in range(main_colors.shape[0])]
+
+# text1 and text2 are output values. Re-assign them to desired value and then pipe the output in the UI
+text1.to(complement_colors[0])
+text2.to(complement_colors[1])
+```
+
+#### Combine Dynamic Values with Wildcards for Prompt Generation
+
+We may have good tools involving wildcards, but perhaps some tokens in the prompt should be dynamically generated (like from determining the complementary color), while the rest can be wildcards.
+
+```python
+import random
+
+# Assume you put the colors from the previous example into the text1 input for this node
+complementary_color = text1
+
+prompt = f"A {complementary_color} __wildcard_obj___ __wildcard_action__ in __wildcard_location__"
+
+# Now take the text2 output and pipe to a wildcard node
+text2.to(prompt)
+```
+
+
 
 
--- python_node.py
+++ python_node.py
@@ -2,11 +2,28 @@ import torch
 from typing import Optional, List, Union
 from io import StringIO
 from contextlib import redirect_stdout, redirect_stderr
-import numpy
-from PIL import Image
+
+try:
+    from .wrapper_class import TensorWrapper
+except ImportError:
+    import sys
+
+    sys.path.append("..")
+    from wrapper_class import TensorWrapper
 
 
 class PythonInterpreter:
+    CODE_PLACEHOLDER = "\n".join(
+        [
+            "# Write your code here.",
+            "# Use image1.tensor to get the raw tensor",
+            "# Don't re-assign input variables using = operator",
+            "# \tInstead, use .to() (like, image1.to(image1 * mask1))"
+            "# \tOr use in-place operators (e.g., -=, +=, *=, **=, //=, /=)",
+            "# \tHowever, you can change the input variables attributes per usual",
+        ]
+    )
+
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -14,7 +31,7 @@ class PythonInterpreter:
                 "python_code": (
                     "STRING",
                     {
-                        "default": "# Write your code here.\n# Use image1.tensor to get the raw tensor.\n# You can't re-assign the input variables.\n# Use in-place operators (e.g., -=, +=, *=, **=, //=, /=)\n\nprint(f'image1 shape: {image1.shape}')\nprint(f'image2 shape: {image2.shape}')\nprint(f'mask1 shape: {mask1.shape}')\n\nif image1 != mask1:\n\tprint('image1 and mask1 are not equal')\n\timage1 = image1 - mask1",
+                        "default": cls.CODE_PLACEHOLDER,
                         "multiline": True,
                     },
                 ),
@@ -39,13 +56,13 @@ class PythonInterpreter:
                 "text1": (
                     "STRING",
                     {
-                        "default": "xd",
+                        "default": "hello world",
                     },
                 ),
                 "text2": (
                     "STRING",
                     {
-                        "default": "xd",
+                        "default": "hello world",
                     },
                 ),
                 "give_full_error_stack": (
@@ -191,307 +208,3 @@ class PythonInterpreter:
             },
             "result": result,
         }
-
-
-class TensorWrapper:
-    def __init__(self, tensor):
-        if not isinstance(tensor, torch.Tensor):
-            if isinstance(tensor, numpy.ndarray):
-                self.tensor = torch.from_numpy(tensor)
-            if isinstance(tensor, Image.Image):
-                self.tensor = torch.tensor(numpy.array(tensor))
-            else:
-                self.tensor = None
-        else:
-            self.tensor = tensor
-
-        self.null_msg = (
-            "No image passed to an input with the same name as this variable"
-        )
-        if self.tensor is not None:
-            self.shape = tensor.shape
-            self.dim = tensor.dim()
-        else:
-            self.shape, self.dim = self.null_msg, self.null_msg
-
-    def subtract(self, other):
-        """
-        1 - A: invert mask
-        RGB/RGBA - RGB/RGBA: subtract pixel values (difference image)
-        RGBA - A: minimum operation on alpha channels
-
-        RGBA/A - number: decrease opacity
-        RGBA/RGB - str: remove text from image
-        RGB - number: decrease intensity
-        """
-        if other == "tensor":
-            if other.tensor.dim() == 3 and self.is_tensor():
-                return self.subtract_mask_from_img(self.tensor, other.tensor)
-
-    def __sub__(self, other):
-        return TensorWrapper(self.subtract(other))
-
-    def __isub__(self, other):
-        self.tensor = self.subtract(other)
-
-    def subtract_mask_from_img(self, img, mask):
-        # RGBA - A
-        if img.shape[3] == 4:
-            subtracted_alphas = torch.min(img[0][:, :, 3], mask[0])
-            return torch.cat(
-                (
-                    img[0][:, :, :3],
-                    subtracted_alphas.unsqueeze(2),
-                ),
-                dim=2,
-            ).unsqueeze(0)
-        # RGB - A
-        else:
-            return torch.cat((img[0][:, :, :3], mask[0].unsqueeze(2)), dim=2).unsqueeze(
-                0
-            )
-
-    def add(self, other):
-        """
-        RGB/RGBA + RGB/RGBA: composite
-
-        A + A: max operation on alpha channels
-        RGB + A: add alpha channel to RGB
-        RGBA + A: max on alpha channels
-        RGB/RGBA + number: increase intensity
-        str + RGB/RGBA/A or RGB/RGBA/A + str: add text to image
-        str which has been multiplied (e.g., "text text text text") + RGB/RGBA/A: add text to image and increase text's size by number of repetitions
-        """
-        if self == "tensor" and other == "tensor":
-            channel_sum = self.shape[3] + other.shape[3]
-            if channel_sum >= 6:
-                return self.add_img_and_img(self.tensor, other.tensor)
-            elif channel_sum > 2:
-                return self.add_img_and_mask(self.tensor, other.tensor)
-            else:
-                return self.add_mask_and_mask(self.tensor, other.tensor)
-
-    def __add__(self, other):
-        return TensorWrapper(self.add(other))
-
-    def __iadd__(self, other):
-        self.tensor = self.add(other)
-
-    def add_img_and_mask(self, img, mask):
-        # RGB/RGBA + A
-        return torch.cat(
-            (
-                img[0][:, :, :3],
-                mask[0].unsqueeze(2),
-            ),
-            dim=2,
-        ).unsqueeze(0)
-
-    def add_mask_and_mask(self, mask1, mask2):
-        # A + A
-        return torch.max(mask1[0], mask2[0]).unsqueeze(0)
-
-    def add_img_and_img(self, img1: torch.Tensor, img2: torch.Tensor):
-        # RGB + RGBA or RGBA + RGB
-        if img1.shape[3] != img2.shape[3] and img1.shape[3] == 3:
-            img1[:, :, :, 3] = 1
-            print(img1.shape, img2.shape)
-            return torch.where(
-                img2[:, :, :, 3] == 0,
-                img2,
-                img1,
-            )
-        if img2.shape[3] != img1.shape[3] and img2.shape[3] == 3:
-            img2[:, :, :, 3] = 1
-            print(img1.shape, img2.shape)
-            return torch.where(
-                img1[:, :, :, 3] == 0,
-                img1,
-                img2,
-            )
-        print(img1.shape, img2.shape)
-        img1 = img1.squeeze(0)
-        img2 = img2.squeeze(0)
-        alpha1 = img1[:, :, 3].unsqueeze(2)
-        print(alpha1.shape, img1.shape, img2.shape)
-        ret = img1[:, :, :3] * alpha1 + img2[:, :, :3] * (1 - alpha1)
-        print(ret.shape)
-        return ret.unsqueeze(0)
-
-    def size(self):
-        if self.tensor is None:
-            return None
-        return self.tensor.size()
-
-    def is_tensor(self):
-        return isinstance(self.tensor, torch.Tensor)
-
-    def resolve(self):
-        # TODO:
-        return self.tensor
-
-    def __repr__(self):
-        return f"ImageTensorWrapper({self.shape})"
-
-    def __str__(self):
-        if self.tensor is not None:
-            return f"ImageTensorWrapper(shape: {self.shape}, dim: {self.dim}, dtype: {self.tensor.dtype}, device: {self.tensor.device})"
-        return "NoneType"
-
-    # def __floor__(self):
-    #     # TODO
-    #     return self.tensor
-
-    # def __pos__(self):
-    #     # Unary positive: +tensor
-    #     # FROM A: convert to image
-    #     pass
-
-    # def __neg__(self):
-    #     # Unary negative: -tensor
-    #     # FROM A: convert to mask
-    #     pass
-
-    # def __abs__(self):
-    #     # absolute value: abs(tensor)
-    #     # ....
-    #     pass
-
-    # def __invert__(self):
-    #     # bitwise NOT: ~tensor
-    #     # FROM A: invert mask
-    #     # FROM RGB: invert image colors (1 - normalized rgb values)
-    #     # FROM RGBA: invert alpha channel
-    #     pass
-
-    # def __round__(self):
-    #     # round to nearest: round(tensor)
-    #     # FROM RGB/RGBA: infer alpha from rgb values
-    #     pass
-
-    def __eq__(self, other: Union[torch.Tensor, str]):
-        # equal: tensor == other
-        # RGB/RGBA/A == "tensor"
-        if isinstance(other, str) and other == "tensor":
-            return self.is_tensor()
-        # FROM RGB/RGBA/A: compare sizes for equality
-        # RGB/RGBA/A == RGB/RGBA/A: compare pixel values for equality
-        if isinstance(other, torch.Tensor):
-            if torch.equal(self.tensor, other):
-                return True
-        return False
-
-    def __ne__(self, other):
-        # not equal: tensor != other
-        # FROM RGB/RGBA/A: compare sizes for inequality
-        return not self.__eq__(other)
-
-    def __lt__(self, other):
-        # less than: tensor < other
-        # FROM RGB/RGBA/A: compare sizes for less than
-        pass
-
-    def __le__(self, other):
-        # less or equal: tensor <= other
-        # FROM RGB/RGBA/A: compare sizes for less or equal
-        pass
-
-    def __gt__(self, other):
-        # greater than: tensor > other
-        # FROM RGB/RGBA/A: compare sizes for greater than
-        pass
-
-    def __ge__(self, other):
-        # greater or equal: tensor >= other
-        # FROM RGB/RGBA/A: compare sizes for greater or equal
-        pass
-
-    # def __mul__(self, other):
-    #     # multiplication: tensor * other
-    #     # A * A: normal matrix multiplication
-    #     # RGB/RGBA * A: one-zero matrix addition
-    #     # RGB/RGBA * number: increase size of image by number
-    #     pass
-
-    # def __truediv__(self, other):
-    #     # division: tensor / other
-    #     # RGB/RGBA/A / number: decrease size of image by number
-    #     # RGB/RGBA/A / (number, number): resize to size of tuple
-    #     # RGB/RGBA/A / RGB/RGBA/A: resize to size of 2nd image, changing aspect ratio if necessary
-    #     pass
-
-    # def __floordiv__(self, other):
-    #     # floor division: tensor // other
-    #     # RGB/RGBA // number: decrease size of image by number, rounding down
-    #     # RGB/RGBA // (number, number): resize to size of tuple, maintaing aspect ratio
-    #     # RGB/RGBA/A // RGB/RGBA/A: resize to size of 2nd image, maintaing aspect ratio via crop or padding
-    #     pass
-
-    # def __pow__(self, other):
-    #     # tensor ** other
-    #     # FROM A to A: matrix power
-    #     # FROM RGB/RGBA to RGB/RGBA: apply gamma correction
-    #     # RGB/RGBA ** number: add self to batch with number copies
-    #     pass
-
-    # def __or__(self, other):
-    #     # bitwise OR: tensor | other
-    #     # pad lower by other pixels
-    #     pass
-
-    # def __xor__(self, other):
-    #     # bitwise XOR: tensor ^ other
-    #     # pad upper by other pixels
-    #     pass
-
-    # def __lshift__(self, other):
-    #     # bitwise left shift: tensor << other
-    #     # pad left by other pixels
-    #     pass
-
-    # def __rshift__(self, other):
-    #     # bitwise right shift: tensor >> other
-    #     # pad right by other pixels
-    #     pass
-
-    # def __getitem__(self, key):
-    #     # tensor[key]
-    #     # RGBA/RGB/A[key]: get key-index item in batch
-    #     pass
-
-    # def __setitem__(self, key, value):
-    #     # tensor[key] = value
-    #     # RGBA/RGB/A[key] = RGBA/RGB/A: set key-index color channel to match value of other
-    #     # RGBA/RGB/A[1] = number: set width to number
-    #     # RGBA/RGB/A[2] = number: set height to number
-    #     pass
-
-    # def __delitem__(self, key):
-    #     # del tensor[key]
-    #     # del RGBA/RGB/A[key]: remove key-index item from batch
-    #     pass
-
-    # def __len__(self):
-    #     # len(tensor)
-    #     # len(RGBA/RGB/A): number of items in batch
-    #     pass
-
-    # def __iter__(self):
-    #     # iter(tensor)
-    #     # iter(RGBA/RGB/A): iterate over items in batch
-    #     pass
-
-    # def __contains__(self, item):
-    #     # item in tensor
-    #     # CHW in BCHW: check if item is in batch
-    #     # (number, number, number) in RGBA/RGB/A: check if rgb value is a term in tensor
-    #     # number in RGBA/RGB/A: check if number is either height or width
-    #     # (number, number) in RGBA/RGB/A: check if a tensor with those dimensions exists in the batch
-    #     # 0 in RGBA/RGB/A: check if there is a hole in the image
-    #     # item in RGBA/RGB/A: check if item is in batch
-    #     pass
-
-    # def __call__(self, *args, **kwargs):
-    #     # tensor(*args, **kwargs)
-    #     # RGBA/RGB/A(*args, **kwargs): apply function to each item in batch
-    #     pass
