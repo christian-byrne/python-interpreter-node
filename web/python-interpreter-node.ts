d6e1f7be967b0ef1383f5f8f597574fccb9f5f7a
christian-byrne <abolkonsky.rem@gmail.com>
2024-09-28 19:27:16 -0700

    Remove console logs

--- web/python-interpreter-node.ts
+++ web/python-interpreter-node.ts
@@ -20,10 +20,11 @@ const createUniqueID = (): string => {
   }
   ACTIVE_EDITORS[id] = true;
   return id;
-}
+};
 
 const PythonInterpreterExtension: ComfyExtension = {
   name: nodeConfig.graphName,
+
   init: async (app: ComfyApp) => {
     document.head.append(
       Object.assign(document.createElement("script"), {
@@ -31,18 +32,17 @@ const PythonInterpreterExtension: ComfyExtension = {
       })
     );
   },
-  setup : async (app: ComfyApp) => {
+
+  setup: async (app: ComfyApp) => {
     const themeSelection = app.ui.settings.addSetting({
       id: "PythonInterpreter.Theme",
       name: "Editor Theme",
       defaultValue: "github_dark",
       type: "combo",
       options: nodeConfig.themes,
-    })
+    });
   },
-  nodeCreated: async (node: LGraphNodeExtension) => {
-    console.debug("[onNodeCreated Handler] Node created:", node);
-  },
+
   beforeRegisterNodeDef: async (
     nodeType: NodeType,
     nodeData: ComfyObjectInfo,
@@ -57,8 +57,6 @@ const PythonInterpreterExtension: ComfyExtension = {
         const nodePrototype: LGraphNodeExtension = this;
         if (nodePrototype.title == nodeConfig.nodeTitle) {
           const editorId = createUniqueID();
-          console.debug("[onNodeCreated Handler] Node created:", nodePrototype);
-          console.debug("[onNodeCreated Handler] Editor ID:", editorId);
           initAceInstance(nodePrototype, editorId);
           createAceDomElements(nodePrototype, editorId);
         }
@@ -67,7 +65,6 @@ const PythonInterpreterExtension: ComfyExtension = {
       // Add a new widget to display the stdout/stderr after execution.
       constructorPrototype.onExecuted = function (data) {
         const node: LGraphNodeExtension = this;
-        console.debug("[onExecuted handler] Node executed:", node);
         if (node.widgets) {
           appendStdoutWidget(node, liteGraph, data);
         }
