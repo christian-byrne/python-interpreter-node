3db0be038211972d8694cc23d94c85671bdff068
christian-byrne <abolkonsky.rem@gmail.com>
2024-09-28 18:54:11 -0700

    Remove vendored files

--- d/ace-builds-master/demo/kitchen-sink/docs/jsx.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export default () => {
-  return (
-    <div variant="p">
-      Keywords here are not highlighted, for example class or instance.
-    </div>
-  );
-}
--- d/ace-builds-master/demo/kitchen-sink/docs/nasal.nas
+++
@@ -1,7 +0,0 @@
-var sayHello = func(names, favorite) {
-  foreach (var name; names) {
-    printf("Hello %s, %s is the best!", name, favorite);
-  }
-}
-
-sayHello(["World", "FlightGear"], "Nasal");
