ac2547ada25643be7cfe4b8030e57059c9da5bd7
christian-byrne <abolkonsky.rem@gmail.com>
2025-01-05 12:19:00 -0700

    Remove demo files off master

--- wiki/code-snippets-from-demos/type_conversions.py
+++
@@ -1,9 +0,0 @@
-print(f"The type of number1 when it was received was {type(number1)}")
-
-if str(number1).isnumeric():
-    number1.to(int(number1))
-else:
-    print(f"number1 was not a number. It was {number1}")
-
-if number1 < 0:
-    number1.to(0)
